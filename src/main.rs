b61cd5ceb66d82494a60896ec1a7d6c7fe392cdf
Josh Vander Hook <josh@vanderhook.info>
2024-01-18 13:54:23 -0600

    et works

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,72 @@
 #![deny(warnings)]
 
+use warp::Filter;
+use spice;
+use spice::SpiceLock;
+use std::sync::{Arc, Mutex};
+use serde::{Serialize, Deserialize};
+
 #[tokio::main]
 async fn main() {
-    let routes = filters::api();
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+
+    //let mut tlskernel = spice::furnsh("./latest_leapseconds.tls");
+    let sl = SpiceLock::try_acquire().unwrap();
+    sl.furnsh("./latest_leapseconds.tls");
+    let tlskernel = 
+        Arc::new(Mutex::new(sl));
+
+
+    let et_time = warp::path!("et")
+        .and(warp::get())
+        .and(warp::query::<EtQuery>())
+        .and(with_kernel(tlskernel))
+        .and_then(get_et_time);
+
+    let readme = warp::path!("readme")
+        .and(warp::fs::file("./README.md"));
+
+    let routes = readme
+        .or(et_time)
+        ;
+
+    println!("Starting server at 127.0.0.1");
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
 
-mod filters {
-    //use super::models::{
-    use warp::Filter;
-
-    pub fn api(
-
-        ) ->
-        impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-        hello()
-            .or(et())
-    }
-
-    pub fn hello() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-        warp::path!("hello")
-            .and(warp::get())
-            .and_then(handlers::hello_handler)
-    }
-
-    pub fn et() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-        warp::path!("et")
-            .and(warp::get())
-            .map(|| format!("ET:") )
-    }
+fn with_kernel(sl: Arc<Mutex<spice::SpiceLock>>) -> impl Filter<Extract = (Arc<Mutex<spice::SpiceLock>>,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || sl.clone())
 }
 
-
-mod handlers
+#[derive(Debug, Deserialize)]
+enum EtFormat{
+    #[serde(rename = "json")]
+    Json,
+}
+#[derive(Debug, Deserialize)]
+struct EtQuery
 {
-    use std::convert::Infallible;
-    pub async fn hello_handler() -> Result<impl warp::Reply, Infallible> {
-        Ok(format!("..."))
-    }
+    f: Option<EtFormat>,
+    m: Option<String>,
+}
+#[derive(Debug, Serialize, Deserialize)]
+struct EtResponse
+{
+    et: f64,
+}
+async fn get_et_time(q:EtQuery, sl_mutex: Arc<Mutex<spice::SpiceLock>>) -> Result<impl warp::Reply, warp::Rejection> {
+    println!("f: {:?}", q.f);
+    println!("f: {:?}", q.m);
+    let lock = sl_mutex.lock().unwrap();
+    let datestr= chrono::Utc::now().format("%Y-%m-%dT%H:%M:%S").to_string();
+    let et:f64 = lock.str2et(datestr.as_str());
+    let response :String = match q.f{
+        Some(EtFormat::Json) => {
+            let er = EtResponse{et};
+            serde_json::to_string(&er).unwrap()
+        },
+        None => {format!("{}", et)},
+    };
+    Ok(response)
 }
