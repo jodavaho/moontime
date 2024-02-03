22eedd76610d3d0083ad13029812d478ac5caf1d
Josh Vander Hook <josh@vanderhook.info>
2024-02-03 05:23:46 -0600

    it works!

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 #![deny(warnings)]
 
+use core::ffi::CStr;
+use std::f64::consts::PI;
+use spice::cstr;
 use warp::Filter;
 use spice;
 use spice::SpiceLock;
 use std::sync::{Arc, Mutex};
 use serde::{Serialize, Deserialize};
+use chrono::Utc;
 
+fn to_rad(deg:f64) -> f64
+{
+    deg / 180.0 * PI
+}
 #[tokio::main]
 async fn main() {
 
@@ -20,9 +28,18 @@ async fn main() {
     //sl.furnsh("data/earth_latest_high_prec.bpc");
     sl.furnsh("data/moon_pa_de440_200625.bpc");
     sl.furnsh("data/moon_de440_220930.tf");
+    sl.furnsh("data/pck00010.tpc");
+
+
 
     let tlskernel = 
         Arc::new(Mutex::new(sl));
+
+    //just test them. 
+    get_et_time(
+        EtQuery{f:Some(EtFormat::Json), m:Some(Utc::now().format("%Y-%m-%dT%H:%M:%S").to_string())},
+        Arc::clone(&tlskernel)).await.unwrap();
+    get_solar_time(Arc::clone(&tlskernel)).await.unwrap();
 
 
     let et_time = warp::path!("et")
@@ -44,12 +61,12 @@ async fn main() {
         .or(et_time)
         ;
 
-    get_solar_time(Arc::clone(&tlskernel)).await.unwrap();
-
     println!("Starting server at 127.0.0.1");
     warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
+
+
 }
 
 fn with_kernel(sl: Arc<Mutex<spice::SpiceLock>>) -> impl Filter<Extract = (Arc<Mutex<spice::SpiceLock>>,), Error = std::convert::Infallible> + Clone {
@@ -78,6 +95,7 @@ async fn get_et_time(q:EtQuery, sl_mutex: Arc<Mutex<spice::SpiceLock>>) -> Resul
     let lock = sl_mutex.lock().unwrap();
     let datestr= chrono::Utc::now().format("%Y-%m-%dT%H:%M:%S").to_string();
     let et:f64 = lock.str2et(datestr.as_str());
+    println!("et: {}", et);
     let response :String = match q.f{
         Some(EtFormat::Json) => {
             let er = EtResponse{et};
@@ -109,26 +127,50 @@ async fn get_solar_time( sl_mutex: Arc<Mutex<spice::SpiceLock>> ) -> Result<impl
     unsafe
     {
         let et_c = et as f64;
+        dbg!(et_c);
         let body_c = 301 as i32;
-        let lon_c = -59 as f64;
-        let type_c = "PLANETOCENTRIC\0".as_ptr() as *mut i8;
+        dbg!(body_c);
+        let lon_c = to_rad(-59_f64) as f64;
+        dbg!(lon_c);
+        let type_of_coord = "PLANETOCENTRIC"; 
+        let type_c = cstr!(type_of_coord);
+        dbg!(type_c);
         const TIMLEN_C:i32 = 256 as i32;
+        dbg!(TIMLEN_C);
         const AMPMLEN_C:i32 = 256 as i32;
-        let hr_c = 0 as *mut i32;
-        let mn_c = 0 as *mut i32;
-        let sc_c = 0 as *mut i32;
+        dbg!(AMPMLEN_C);
+        let mut hr_c:i32 = 0;
+        let hr_cp = &mut hr_c as *mut i32;
+        dbg!(hr_c);
+        let mut mn_c:i32 = 0;
+        let mn_cp = &mut mn_c as *mut i32;
+        dbg!(mn_c);
+        let mut sc_c:i32 = 0;
+        let sc_cp = &mut sc_c as *mut i32;
+        dbg!(sc_c);
         //make a buffer for the time string
-        let mut time_c = [0u8; TIMLEN_C as usize];
-        let time_c_ptr = time_c.as_mut_ptr() as *mut i8;
-        let mut ampm_c = [0u8; AMPMLEN_C as usize];
-        let ampm_c_ptr = ampm_c.as_mut_ptr() as *mut i8;
-        spice::c::et2lst_c(et_c, body_c, lon_c, type_c, TIMLEN_C, AMPMLEN_C, hr_c, mn_c, sc_c, time_c_ptr, ampm_c_ptr);
-        let time_c_str = std::ffi::CStr::from_ptr(time_c_ptr);
-        let ampm_c_str = std::ffi::CStr::from_ptr(ampm_c_ptr);
-        let time_c_str = time_c_str.to_str().unwrap();
-        let ampm_c_str = ampm_c_str.to_str().unwrap();
-        let output_time_string = format!("{} {}", time_c_str, ampm_c_str);
+        let mut time_c = [0i8; TIMLEN_C as usize];
+        dbg!(time_c);
+        let mut ampm_c = [0i8; AMPMLEN_C as usize];
+        dbg!(ampm_c);
+        spice::c::et2lst_c(
+            et_c, 
+            body_c, 
+            lon_c,
+			type_c,
+			TIMLEN_C,
+			AMPMLEN_C,
+			hr_cp,
+			mn_cp,
+			sc_cp,
+			time_c.as_mut_ptr(),
+			ampm_c.as_mut_ptr()
+        );
+        let time = CStr::from_ptr(time_c.as_ptr()).to_str().unwrap();
+        let ampm = CStr::from_ptr(ampm_c.as_ptr()).to_str().unwrap();
+        eprintln!("time: {} {}", time, ampm);
+        eprintln!("DONE!");
         //cleanup
-        Ok(output_time_string)
+        Ok("done")
     }
 }
